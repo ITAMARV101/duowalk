f0626106022b37a4b9c03c0e8a2f62db650e4a11
Student <Student@yarkon.co.il>
2025-11-26 10:33:20 +0200

    firebase (fixed)

--- app/src/main/java/com/example/duowalk/FriendsActivity.java
+++ app/src/main/java/com/example/duowalk/FriendsActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+
 public class FriendsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
