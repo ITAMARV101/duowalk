02fdc2ee2cf0ca5041c6299b8f13fc39646e5f12
Marv <77895162+ITAMARV101@users.noreply.github.com>
2025-12-12 00:47:29 +0200

    FirebaseUtils, AuthActivity, Sign up java -> no login

--- app/src/main/java/com/sport/duowalk/FriendsActivity.java
+++ app/src/main/java/com/sport/duowalk/FriendsActivity.java
@@ -3,6 +3,7 @@ package com.sport.duowalk;
 import android.os.Bundle;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.duowalk.R;
 
 public class FriendsActivity extends AppCompatActivity {
     @Override
